da0a6ee7ad992ea4f783cc864c1a16f04c3b579e
Anurag_Ponnada <apanuragp7@gmail.com>
2023-10-31 00:34:35 +0530

    what:added some code to make it prod ready build

--- functions/server.js
+++ functions/server.js
@@ -1,18 +1,21 @@
 var express = require('express');//require express framework
 var fs = require('fs') //require filesystem object
 const app = express();
+const router = express.Router();
+const serverless = require('serverless-http');
 
 const PORT = 5050;
-app.get("/getRestaurants",(req,res)=>{
+router.get("/getRestaurants",(req,res)=>{
         console.log(res.status)
-    fs.readFile(__dirname+ "/" + "restaurantsData.json", 'utf-8',function(err,data){
+    fs.readFile(__dirname+ "/" + "restaurantsData.json", 'utf-8',async function(err,data){
         // var d = await fetch('http://localhost:8080/getRestaurants');
         res.send(data)
         
     })
 })
 
-app.listen(PORT,function(){
-    console.log(`api listening at http://localhost:${PORT}`)
-})
-
+router.use('/.netlify/functions/server',router)
+// router.listen(PORT,function(){
+//     console.log(`api listening at http://localhost:${PORT}`)
+// })
+module.exports.handler = serverless(app);

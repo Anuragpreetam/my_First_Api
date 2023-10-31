a7f4bd82398cc0efcf5b3cfe9e9d2e2154dce67a
Anurag_Ponnada <apanuragp7@gmail.com>
2023-10-31 19:38:10 +0530

    initial commit

--- functions/server.js
+++ functions/server.js
@@ -4,18 +4,4 @@ const app = express();
 const router = express.Router();
 const serverless = require('serverless-http');
 
-const PORT = 5050;
-router.get("/getRestaurants",(req,res)=>{
-        console.log(res.status)
-    fs.readFile(__dirname +'restaurantsData.json', 'utf-8',function(err,data){
-        // var d = await fetch('http://localhost:8080/getRestaurants');
-        res.send(data)
-        
-    })
-})
 
-router.use('.netlify/functions/server',router)
-// router.listen(PORT,function(){
-//     console.log(`api listening at http://localhost:${PORT}`)
-// })
-module.exports.handler = serverless(app);
